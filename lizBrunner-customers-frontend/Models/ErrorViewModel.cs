158074a3bfde051d3f7524da855578e609aa9e50
iphilippe <phpazinatto@gmail.com>
2021-09-18 01:08:15 -0300

    Commit inicial do projeto limpo

---
+++ lizBrunner-customers-frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace lizBrunner_customers_frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
